743570c8eb2cd7ab84328101e1c73d6c4b50dc18
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-04 17:57:08 +0300

    refactor: update ClientHeader to use local state for notifications and profile instead of routing

--- components/layout/client/ClientHeader.tsx
+++ components/layout/client/ClientHeader.tsx
@@ -2,10 +2,14 @@
 
 import { useRouter } from 'next/navigation';
 import { Bell, User, Clapperboard } from 'lucide-react'; // Lucide icons
+import { useState } from 'react';
+import NotificationPanel from '@/app/client/notifications/page';
+import ProfilePage from '@/app/client/profile/page';
 
 export default function ClientHeader() {
   const router = useRouter();
-
+  const [showNotifications, setShowNotifications] = useState(false);
+  const [showProfile, setShowProfile] = useState(false);
   return (
     <header className="w-full fixed top-0 z-50 px-6 py-4 bg-transparent flex justify-between items-center">
       <div className="flex items-center gap-2">
@@ -13,21 +17,27 @@ export default function ClientHeader() {
         <h1 className="text-white font-bold text-xl">MovieMania</h1>
       </div>
       <div className="flex gap-3">
+         <button
+            onClick={() => setShowNotifications(true)}
+            className="bg-white text-black p-2 rounded-full hover:bg-gray-200 transition"
+            title="Notifications"
+          >
+            <Bell className="w-5 h-5" />
+          </button>
         <button
-          onClick={() => router.push('client/notifications')}
-          className="bg-white text-black p-2 rounded-full hover:bg-gray-200 transition"
-          title="Notifications"
-        >
-          <Bell className="w-5 h-5" />
-        </button>
-        <button
-          onClick={() => router.push('/client/profile')}
+          onClick={() => setShowProfile(true)}
           className="bg-white text-black p-2 rounded-full hover:bg-gray-200 transition"
           title="Profile"
         >
           <User className="w-5 h-5" />
         </button>
       </div>
+      {showNotifications && (
+        <NotificationPanel onClose={() => setShowNotifications(false)} />
+      )}
+      {showProfile&&(
+        <ProfilePage onClose={() => setShowProfile(false)} />
+      )}
     </header>
   );
 }
