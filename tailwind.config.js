e1bbb6aa9ec69087f9269496c49eca9593b01487
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-04 17:56:29 +0300

    feat: add Tailwind CSS configuration for styling components and pages

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+   './app/**/*.{js,ts,jsx,tsx}',     // Next.js App Router
+    './pages/**/*.{js,ts,jsx,tsx}',   // (if using Pages too)
+    './components/**/*.{js,ts,jsx,tsx}', // Your Button lives here
+   
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
