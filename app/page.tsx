6be5b9db2c018f3690da968c53f20d8f8b4d8eb9
yeab-B <yeabsirabehailu92@gmail.com>
2025-07-04 10:02:02 +0300

    Implement code changes to enhance functionality and improve performance

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to MovieHub</h1>
-      <p>Your one-stop solution for all movie-related information.</p>
+    <main
+      className="min-h-screen flex flex-col items-center justify-center text-white bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/BG.png')",
+      }}
+    >
       <Image
-        src="/images/movie-poster.jpg"
+        src="/movie-poster.png"
         alt="Movie Poster"
         width={500}
         height={750}
       />
+      <p className="font-bold text-2xl text-center mt-6">
+        Find{" "}
+        <span className="bg-gradient-to-r from-[#D6C7FF] to-[#AB8BFF] bg-clip-text text-transparent">
+          Movies
+        </span>{" "}
+        You’ll Love Without the Hassle
+      </p>
     </main>
   );
 }
